d705189d4c60d3c017e13d8d7deb081d79a1726b
Aravind Raveendran <aravind.raveendran@dolby.com>
2022-11-18 15:18:27 +1100

    Show popover for Token Info

---
+++ examples/mobile/src/screens/DemoToken/DemoToken.style.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  infoIconWrapper: {
+    position: 'absolute',
+    right: 0,
+  },
+});
+export default styles;
