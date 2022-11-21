948d1eaf0bb924bd4392dab933e320e08a84f5cc
Aravind Raveendran <aravind.raveendran@dolby.com>
2022-11-22 09:38:01 +1100

    Fix toast alignment in Android

--- examples/mobile/src/screens/Conference/Conference.tsx
+++ examples/mobile/src/screens/Conference/Conference.tsx
@@ -14,7 +14,7 @@ import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {StackActions, useNavigation} from '@react-navigation/native';
 import React, {useEffect, useMemo, useState} from 'react';
 import {useIntl} from 'react-intl';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, LayoutChangeEvent} from 'react-native';
 
 import {Routes} from '../../types/routes.types';
 import {getShareURL} from '../../utils/share.util';
@@ -31,6 +31,11 @@ export const Conference = ({route, navigation}) => {
   const [isListeningRecordCompletion, setIsListeningRecordCompletion] =
     useState(false);
   const intl = useIntl();
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [conferenceContentBottomPosition, setConferenceContentBottomPosition] =
+    useState(0);
+  const [conferenceContentBottomOffset, setConferenceContentBottomOffset] =
+    useState(0);
 
   useEffect(() => {
     switch (status) {
@@ -60,53 +65,73 @@ export const Conference = ({route, navigation}) => {
 
   setIsConferenceOwner(meetingOwner);
 
+  const onConferenceContentLayout = (event: LayoutChangeEvent) => {
+    const {y, height} = event.nativeEvent.layout;
+    setConferenceContentBottomPosition(y + height);
+  };
+
+  const onScreenLayout = (event: LayoutChangeEvent) => {
+    const {height} = event.nativeEvent.layout;
+    setScreenHeight(height);
+  };
+
+  useEffect(() => {
+    setConferenceContentBottomOffset(
+      screenHeight - conferenceContentBottomPosition - 15,
+    );
+  }, [screenHeight, conferenceContentBottomPosition]);
+
   return (
-    <BottomSheetModalProvider>
-      <Layout testID="ConferenceScreen">
-        {
-          (
-            <SafeAreaView style={styles.wrapper}>
-              {conference && (
-                <>
-                  <TopActionBar title={conference.alias ?? ''} />
-                  {isRecordingModeActive && <RecordingActionBar />}
-                  <View style={styles.wrapper}>
-                    <ConferenceContent shareURL={shareURL} />
-                  </View>
-                  <ActionBar
-                    leaveConferenceNav={() => {
-                      if (isRecordingModeActive) handleStopRecording();
-                      navigation.dispatch(
-                        StackActions.replace(Routes.ConferenceLeft, {
-                          userName,
-                          meetingName,
-                        }),
-                      );
-                    }}
-                    moreOptionsNav={() =>
-                      navigation.navigate(Routes.SelectTheme)
-                    }
-                    shareURL={shareURL}
-                  />
-                </>
-              )}
-              <Toast
-                variant={isError ? 'error' : 'success'}
-                text={
-                  isError
-                    ? intl.formatMessage({id: 'messageRecordingFailed'})
-                    : intl.formatMessage({id: 'messageRecordingSuccessful'})
-                }
-                visible={toastVisible}
-                offset={120}
-                onClose={() => {
-                  setToastVisible(false);
-                }}
-              />
-            </SafeAreaView>
-          ) as any
-        }
-      </Layout>
-    </BottomSheetModalProvider>
+    <View onLayout={onScreenLayout} style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <Layout testID="ConferenceScreen">
+          {
+            (
+              <SafeAreaView style={styles.wrapper}>
+                {conference && (
+                  <>
+                    <TopActionBar title={conference.alias ?? ''} />
+                    {isRecordingModeActive && <RecordingActionBar />}
+                    <View
+                      onLayout={onConferenceContentLayout}
+                      style={styles.wrapper}>
+                      <ConferenceContent shareURL={shareURL} />
+                    </View>
+                    <ActionBar
+                      leaveConferenceNav={() => {
+                        if (isRecordingModeActive) handleStopRecording();
+                        navigation.dispatch(
+                          StackActions.replace(Routes.ConferenceLeft, {
+                            userName,
+                            meetingName,
+                          }),
+                        );
+                      }}
+                      moreOptionsNav={() =>
+                        navigation.navigate(Routes.SelectTheme)
+                      }
+                      shareURL={shareURL}
+                    />
+                  </>
+                )}
+              </SafeAreaView>
+            ) as any
+          }
+        </Layout>
+        <Toast
+          variant={isError ? 'error' : 'success'}
+          text={
+            isError
+              ? intl.formatMessage({id: 'messageRecordingFailed'})
+              : intl.formatMessage({id: 'messageRecordingSuccessful'})
+          }
+          visible={toastVisible}
+          offset={conferenceContentBottomOffset}
+          onClose={() => {
+            setToastVisible(false);
+          }}
+        />
+      </BottomSheetModalProvider>
+    </View>
   );
 };
