a11db60913509058c33b13cef09e58ae23c7c110
Liubov Danilina <liubov.danilina@dolby.com>
2022-11-21 11:31:32 +1100

    DemoToken screen link added

--- examples/mobile/src/screens/DemoToken/DemoToken.tsx
+++ examples/mobile/src/screens/DemoToken/DemoToken.tsx
@@ -6,7 +6,7 @@ import {
 } from '@dolbyio/comms-uikit-react-native';
 import {StackActions} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {Linking, SafeAreaView, View} from 'react-native';
 
 import {ConferenceCreateHeader} from '../../components/ConferenceCreateHeader/ConferenceCreateHeader';
 import Footer from '../../components/Footer';
@@ -54,13 +54,31 @@ export const DemoToken = ({navigation}) => {
         <Text type="h2" align="center" color="white" style={{paddingTop: 40}}>
           Provide a demo token
         </Text>
-        <Text
-          type="paragraph"
-          align="center"
-          color="secondary.200"
-          style={{paddingTop: 8, paddingBottom: 24}}>
-          In order to use this app you need to provide a token from your Dolby
-          IO dashboard.
+        <Text align="center">
+          <Text
+            id="provideTokenDescription"
+            type="paragraph"
+            align="center"
+            color="secondary.200"
+            style={{paddingTop: 8, paddingBottom: 24}}
+          />
+          <Text
+            id="dolbyIODashboard"
+            type="paragraph"
+            align="center"
+            color="purple.400"
+            style={{paddingTop: 8, paddingBottom: 24}}
+            onPress={() => {
+              return Linking.openURL('https://dashboard.dolby.io/');
+            }}
+          />
+          <Text
+            type="paragraph"
+            align="center"
+            color="secondary.200"
+            style={{paddingTop: 8, paddingBottom: 24}}>
+            .
+          </Text>
         </Text>
         <TokenTab onSelectionChange={onTabChange} />
         <View style={{height: 40}} />
