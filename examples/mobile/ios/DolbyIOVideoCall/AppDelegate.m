05449601462254d708f1f00cc2818bf11b9023d7
Liubov Danilina <liubov.danilina@dolby.com>
2023-06-08 14:37:21 +1000

    RN 0.71.3 with RN permissions ^3.8.0

--- examples/mobile/ios/DolbyIOVideoCall/AppDelegate.m
+++ examples/mobile/ios/DolbyIOVideoCall/AppDelegate.m
@@ -1,100 +1,36 @@
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-#import <React/RCTLinkingManager.h>
-
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
-#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [self clearKeychainIfNecessary];
-  
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
+  self.moduleName = @"RnDiffApp";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"mobile"
-                                            initialProperties:nil];
-
-  rootView.backgroundColor = [UIColor colorWithRed:0.078431372549019607 green:0.078431372549019607 blue:0.10196078431372549 alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  [NSThread sleepForTimeInterval:0.500];
-  return YES;
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
-- (BOOL)application:(UIApplication *)application
-   openURL:(NSURL *)url
-   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
 {
-  return [RCTLinkingManager application:application openURL:url options:options];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
-{
-  return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-}
-
-// MARK: Keychain Utility
-
-/// Deletes all Keychain items accessible by this app if this is the first time the user launches the app
-- (void)clearKeychainIfNecessary
-{
-  // Checks wether or not this is the first time the app is run
-  if ([[NSUserDefaults standardUserDefaults] boolForKey:@"FIRST_RUN"] == NO) {
-    // Set the appropriate value so we don't clear next time the app is launched
-    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"FIRST_RUN"];
-    
-    NSArray *secItemClasses = @[
-      (__bridge id)kSecClassGenericPassword,
-      (__bridge id)kSecClassInternetPassword,
-      (__bridge id)kSecClassCertificate,
-      (__bridge id)kSecClassKey,
-      (__bridge id)kSecClassIdentity
-    ];
-    
-    // Maps through all Keychain classes and deletes all items that match
-    for (id secItemClass in secItemClasses) {
-      NSDictionary *spec = @{(__bridge id)kSecClass: secItemClass};
-      SecItemDelete((__bridge CFDictionaryRef)spec);
-    }
-  }
+  return true;
 }
 
 @end
